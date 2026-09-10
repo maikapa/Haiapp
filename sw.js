a8e21f7f09ea5ca006488e099ee8e47aa33477cc
maikapa <lodilodsn@gmail.com>
2026-09-10 08:48:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Version bei jedem Deployment um eins hochzaehlen.
 // Nur diese Zahl aendern - der Rest bleibt gleich.
-var APP_VERSION = 2;
+var APP_VERSION = 4;
 
 var CACHE = 'hai-v' + APP_VERSION;
 var FILES = [
