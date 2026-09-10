46975a603190dd514524a26c9b959ae5d54be935
maikapa <lodilodsn@gmail.com>
2026-09-10 08:31:19 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+// Version bei jedem Deployment um eins hochzaehlen.
+// Nur diese Zahl aendern - der Rest bleibt gleich.
+var APP_VERSION = 2;
+
+var CACHE = 'hai-v' + APP_VERSION;
+var FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) { return c.addAll(FILES); })
+          .then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k !== CACHE) return caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+// Spieldatei: erst Netz, dann Cache - so kommen neue Versionen an,
+// und ohne Netz laeuft das Spiel trotzdem.
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () {
+      return caches.match(e.request).then(function (hit) {
+        return hit || caches.match('./index.html');
+      });
+    })
+  );
+});
